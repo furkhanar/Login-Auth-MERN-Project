1ac55118c4e3a67e0b2ede5004e078597c43723e
furkhan ar <mdfurkhanar@gmail.com>
2026-05-11 15:36:54 +0530

    fixed client folder issue

---
+++ client/src/pages/Login.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import "../styles/Auth.css";
+
+function Login() {
+
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/login",
+        form
+      );
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login Successful");
+
+    } catch (error) {
+      alert(error.response.data);
+    }
+  };
+
+  return (
+    <div className="auth-container">
+
+      <div className="auth-card">
+
+        <h2>Login</h2>
+
+        <form onSubmit={handleSubmit}>
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleChange}
+          />
+
+          <button type="submit">
+            Login
+          </button>
+
+        </form>
+
+        <div className="auth-link">
+          Don't have account? <Link to="/register">Register</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/Register.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import "../styles/Auth.css";
+
+function Register() {
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const res = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        form
+      );
+
+      alert(res.data);
+
+    } catch (error) {
+      alert(error.response.data);
+    }
+  };
+
+  return (
+    <div className="auth-container">
+
+      <div className="auth-card">
+
+        <h2>Register</h2>
+
+        <form onSubmit={handleSubmit}>
+
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Name"
+            onChange={handleChange}
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Email"
+            onChange={handleChange}
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            onChange={handleChange}
+          />
+
+          <button type="submit">
+            Register
+          </button>
+
+        </form>
+
+        <div className="auth-link">
+          Already have account? <Link to="/">Login</Link>
+        </div>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Register;
