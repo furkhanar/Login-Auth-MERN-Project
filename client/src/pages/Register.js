0158bb91a5bfb20c8f902d3b3fa81c7ab4a06c47
furkhan ar <mdfurkhanar@gmail.com>
2026-05-13 00:29:56 +0530

    updated login ui

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -25,7 +25,7 @@ function Register() {
     try {
 
       const res = await axios.post(
-        "http://localhost:5000/api/auth/register",
+        "https://login-auth-mern-project.onrender.com/api/auth/register",
         form
       );
 
